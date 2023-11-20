83091bd1954a160b7e7c4a396dbf82bdb305459a
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-20 10:37:39 -0500

    1

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+let milkCounter = document.querySelector("#bottleCount")[0]; // milk counter
+let sellMilkButton = document.querySelector("#milkMoney")[0]; // button
+let totalCount = document.querySelector("#total"); // total money earned
+let hireWorkButton = document.querySelector("#hireWorker"); // hires a worker
+let buyCowButton = document.querySelector("#buyCow"); // buys a cow
+let beCeoButton = document.querySelector("#beCeo"); // become a ceo
+let cowPlanetButton = document.querySelector("#cowPlanet"); // buys a planet
+let multiverseButton = document.querySelector("#multiverse"); // rule the multiverse
+let workerNumber = document.querySelector("#workerCount"); //number of workers
+let workerBottles = document.querySelector("#bottleCount1"); // 1 worker equals 1 bottle that sells for $25.00
+let cowNumber = document.querySelector
+
+
 // Each bottle sells for $.50
 // counter increases per # of bottles and totals x .05 per bottle
+let counter = 0;
+
+
+
+
+
+
 
 
 // 1 worker = 1 bottle per sec // Hire 1 worker for $25
@@ -8,5 +28,5 @@
 // 1 building = 100 bottles per sec // Be CEO for $1,000
 // 1 planet = 1000 bottles per sec // Buy planet for $50,000
 // 1 multiverse = 10,000 bottles per sec // Buy multiverse for $1,000,000
-// 1 golden cow =   bottles per sec // Buy the golden cow for $100,000,000
+// 1 golden cow =  black cow turns into a gold cow when you reach $1B
 
