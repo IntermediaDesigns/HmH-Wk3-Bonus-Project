9d52217fea08af0d68de25efd77a171496602283
IntermediaDesigns <intermediadesignsllc@gmail.com>
2023-11-19 19:33:11 -0500

    1

---
+++ index.js
@@ -0,0 +1,12 @@
+// Each bottle sells for $.50
+// counter increases per # of bottles and totals x .05 per bottle
+
+
+// 1 worker = 1 bottle per sec // Hire 1 worker for $25
+// 1 cow = 2 bottles per sec // Buy 1 cow for $40
+// 1 farm = 10 bottles per sec // Buy 1 farm for $150
+// 1 building = 100 bottles per sec // Be CEO for $1,000
+// 1 planet = 1000 bottles per sec // Buy planet for $50,000
+// 1 multiverse = 10,000 bottles per sec // Buy multiverse for $1,000,000
+// 1 golden cow =   bottles per sec // Buy the golden cow for $100,000,000
+
